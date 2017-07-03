9f3e223eb5936bab983612b1dd4255528e45c754
Chris Jim <chrisjim316@gmail.com>
2017-07-03 14:03:03 +0800

    Create index3.js

---
+++ 05-JavaScript/index3.js
@@ -0,0 +1,70 @@
+Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+// Only change code below this line
+var myFirstName = "Chris";
+var myLastName = "Jim";
+
+Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:
+var myStr ="I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+Change the provided string from double to single quotes and remove the escaping.
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+Assign the following three lines of text into the single variable myStr using escape sequences.
+var myStr="FirstLine\n\\SecondLine\\\rThirdLine"; // Change this line
+
+Build myStr from the strings "This is the start. " and "This is the end." using the + operator.
+// Example
+var ourStr = "I come first. " + "I come second.";
+// Only change code below this line
+var myStr = "This is the start. " +  "This is the end.";
+
+Build myStr over several lines by concatenating these two strings:
+"This is the first sentence. " and "This is the second sentence." using the += operator.
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+// Only change code below this line
+var myStr = "This is the first sentence. "; 
+myStr += "This is the second sentence.";
+
+Set myName to a string equal to your name and build myStr with myName between the strings "My name is " and " and I am swell!"
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+// Only change code below this line
+var myName = "Chris Jim";
+var myStr = "My name is" + myName + " and I am swell!";
+
+Set someAdjective and append it to myStr using the += operator.
+// Example
+var anAdjective = "awesome!";
+var ourStr = "Free Code Camp is ";
+ourStr += anAdjective;
+// Only change code below this line
+var someAdjective = "really fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective; 
+
+Use the .length property to count the number of characters in the lastName variable and assign it to lastNameLength.
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+firstNameLength = firstName.length;
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+// Only change code below this line.
+lastNameLength = lastName.length;
+
+
+
+
+
+
+
+
+
+
