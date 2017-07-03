66a01b94d9a03dd4d1864b0b28be89aa012ff863
Chris Jim <chrisjim316@gmail.com>
2017-07-03 14:07:54 +0800

    Create index4.js

---
+++ 05-JavaScript/index4.js
@@ -0,0 +1,61 @@
+Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+firstLetterOfFirstName = firstName[0];
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+Correct the assignment to myStr to achieve the desired effect.
+// Setup
+var myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Fix Me
+
+Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line.
+var thirdLetterOfLastName = lastName;
+
+Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
+Use bracket notation to find the last character in the lastName variable.
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
+
+Use bracket notation to find the second-to-last character in the lastName string.
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+// Setup
+var lastName = "Lovelace";
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+
+
+
+
+
+
+
+
