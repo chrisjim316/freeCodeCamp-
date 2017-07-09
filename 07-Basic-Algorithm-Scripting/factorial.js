5dab3f6c3b67469f2d54318ea7e4592dbbb181db
Chris Jim <chrisjim316@gmail.com>
2017-07-09 23:17:58 +0800

    update format

--- 07-Basic-Algorithm-Scripting/factorial.js
+++ 07-Basic-Algorithm-Scripting/factorial.js
@@ -1,16 +1,9 @@
-Instruction: 
-
-Return the factorial of the provided integer.
-
-If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
-
-Factorials are often represented with the shorthand notation n!
-
-For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
-
-
-
-Recursive Code Solution: 
+/*
+ *Return the factorial of the provided integer.
+ *If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+ *Factorials are often represented with the shorthand notation n!
+ *For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+ */
 
 function factorialize(num) {
   if (num <= 0){
@@ -20,4 +13,5 @@ function factorialize(num) {
   }
 }
 
+//test method 
 factorialize(5);
