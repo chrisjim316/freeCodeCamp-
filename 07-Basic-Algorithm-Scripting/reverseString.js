f19d6e78ddc439d6ae7511b2db120036cf21c669
Chris Jim <chrisjim316@gmail.com>
2017-07-09 23:14:43 +0800

    Create reverseString.js

---
+++ 07-Basic-Algorithm-Scripting/reverseString.js
@@ -0,0 +1,14 @@
+Instruction: 
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+
+
+Code Solution: 
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
