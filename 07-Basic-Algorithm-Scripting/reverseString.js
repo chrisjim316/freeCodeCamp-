2e0f1d091c3439b169f417dd9414ccd471e42e2a
Chris Jim <chrisjim316@gmail.com>
2017-07-09 23:19:26 +0800

    update to new formatting

--- 07-Basic-Algorithm-Scripting/reverseString.js
+++ 07-Basic-Algorithm-Scripting/reverseString.js
@@ -1,14 +1,14 @@
-Instruction: 
-
-You may need to turn the string into an array before you can reverse it.
-
-Your result must be a string.
-
-
-Code Solution: 
+/* 
+ * You may need to turn the string into an array before you can reverse it.
+ * Your result must be a string.
+ * You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+ * We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+ * We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
+ */
 
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
+//test method 
 reverseString("hello");
