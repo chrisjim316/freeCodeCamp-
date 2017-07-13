626c45153974592b30377ad5cc87d4531137ea80
Chris Jim <chrisjim316@gmail.com>
2017-07-13 22:17:36 +0800

    Create largNumArr.js

---
+++ 07-Basic-Algorithm-Scripting/largNumArr.js
@@ -0,0 +1,24 @@
+/*
+ * Return an array consisting of the largest number from each provided sub-array. 
+ * For simplicity, the provided array will contain exactly 4 sub-arrays.
+ * Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+ */
+ 
+ 
+function largestOfFour(arr) {
+  var results = [];
+  for(var r = 0; r < arr.length; r++){
+    var largestNum = arr[r][0]; 
+    for(var c = 1; c < arr[r].length; c++){
+      if(arr[r][c] > largestNum){
+        largestNum = arr[r][c];
+      }
+    }
+    results[r] = largestNum; 
+  }
+  // You can do this!
+  return results;
+}
+
+//test method 
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
