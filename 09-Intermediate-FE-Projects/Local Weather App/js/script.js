e688c516ab9176277a3c5eeeb42086d2b5a0e728
Chris Jim <chrisjim316@gmail.com>
2017-07-24 23:25:45 +0800

    make button click event to disappear and get weather

---
+++ 09-Intermediate-FE-Projects/Local Weather App/js/script.js
@@ -0,0 +1,32 @@
+var api = "https://fcc-weather-api.glitch.me/api/current?";
+var temp;
+
+$(document).ready(function() {
+
+  $("#getWeather").on("click", function() {
+    /* make Check Weather button disappear when clicked */
+    document.getElementById("getWeather").style.display = "none";
+    /* get the user's consent to use his/her location */
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = "lat=" + position.coords.latitude;
+        var long = "lon=" + position.coords.longitude;
+        var urlString = api + lat + '&' + long;
+        getWeather(urlString);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }); //end getWeather function
+  
+});  
+  
+function getWeather(urlString) {
+  $.ajax({
+    url: urlString,  dataType: "json", success: function (result) {
+      temp = Math.round(result.main.temp * 10) / 10;
+      $("#temp").text(temp + " " + String.fromCharCode(176));
+      $("#tempunit").text("C");
+    }
+  });
+}
