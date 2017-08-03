b6572973937e08eac41bb79f69cb01e98279eaa5
Chris Jim <chrisjim316@gmail.com>
2017-08-03 16:36:51 +0800

    add keydown event

--- 09-Intermediate-FE-Projects/Wikipedia Viewer/js/script.js
+++ 09-Intermediate-FE-Projects/Wikipedia Viewer/js/script.js
@@ -1,4 +1,17 @@
-var randomWikiAPI="https://en.wikipedia.org/wiki/Special:Random";
+var search = document.getElementById("search");
+var response = "";
+var searchURL = "";
 
+var randomWikiAPI="https://en.wikipedia.org/wiki/Special:Random";
 $(document).ready(function() {
+});
+
+/* function provides an event listener for the search box, and detects the Enter key. On Enter key, do
+* an article query on Wikipedia using user's search criteria. */
+$("search").keydown(function (event) {
+  if (event.keyCode === 13) {                // test for Enter key pressed (ASCII 13).
+		search.value = "";                       // clear search box.
+		search();                                // run Wikipedia search.
+		return false;                            // return prevents premature Submit of input text box.
+	} 
 });
