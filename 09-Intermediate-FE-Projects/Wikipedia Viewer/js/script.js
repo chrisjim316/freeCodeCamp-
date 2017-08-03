7db7232ebb3e0208fcd76ef009f7ba42dbcef85e
Chris Jim <chrisjim316@gmail.com>
2017-08-03 16:38:12 +0800

    add search method using ajax

--- 09-Intermediate-FE-Projects/Wikipedia Viewer/js/script.js
+++ 09-Intermediate-FE-Projects/Wikipedia Viewer/js/script.js
@@ -10,8 +10,36 @@ $(document).ready(function() {
 * an article query on Wikipedia using user's search criteria. */
 $("search").keydown(function (event) {
   if (event.keyCode === 13) {                // test for Enter key pressed (ASCII 13).
-		search.value = "";                       // clear search box.
-		search();                                // run Wikipedia search.
-		return false;                            // return prevents premature Submit of input text box.
-	} 
+     search.value = "";                       // clear search box.
+     search();                                // run Wikipedia search.
+     return false;                            // return prevents premature Submit of input text box.
+  }
 });
+
+
+/* function search() makes the actual JSONP query to Wikipedia.*/
+function search() {
+  searchURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + $("search").val() + "&callback=?";
+	// srsearch -> get all search results based on keyword search.
+  // by default 10 search results are returned.  
+  $.ajax({
+    url: searchURL,
+    data: queryData,
+    dataType: "jsonp",
+    contentType: "application/json; charset=utf-8",
+    async: false,
+    type: "GET",
+    headers: {
+      'Api-User-Agent': 'Example/1.0'
+    },
+    success: function(data) {
+      // First we make sure no previous results are showing.
+      var results = data.query.pages;
+      //generate html for each search result 
+      for(var res in results) {
+        response = '<div class="well"><div id="title"><h1>' + res.title + '</h1></div>' + '<div id="body"><h2>' + res.extract + '</h2></div></div>';
+        $("#response-area").append(response);
+      }  
+    }
+  });
+}
